8e918e85c49f37fbf0dbbaba6fbc4e983bbff81d
Yechiel <springer058@gmail.com>
2025-12-07 15:58:45 +0200

    eshay commit

---
+++ game_logic/game.js
@@ -0,0 +1,15 @@
+// a. create_player(name:str) -> dict 
+// b. init_game()->dict 
+// c. play_round(p1:dict,p2:dict) 
+
+// function createPlayer(name){
+//     const player1 = {
+//         name,
+//         hand : [],
+
+
+
+//     }
+
+// }
+
