f683663653cdf2d9c6bc2b87afbc5f1102fc910c
Yechiel <springer058@gmail.com>
2025-12-07 18:01:51 +0200

    gamelogic

--- game_logic/game.js
+++ game_logic/game.js
@@ -1,15 +1,76 @@
-// a. create_player(name:str) -> dict 
-// b. init_game()->dict 
-// c. play_round(p1:dict,p2:dict) 
+import {compareCards,createDeck,shuffle,createCard} from '../utils/deck.js'  
+ 
+ 
 
-// function createPlayer(name){
-//     const player1 = {
-//         name,
-//         hand : [],
+function createPlayer(name){
+    const player1 = {
+        name,
+        hand : [],
+        wonPile : [],
+    }
+    return player1
+}
+function createPlayerai(){
+    const player2 = {
+        name:"AI",
+        hand:[],
+        wonPile:[]
+    }
+    return player2
+} 
+
+
+function initGame(){
+    const player1 = createPlayer("yechiel")
+    const player2 = createPlayerai()
+    const decknotshaf = createDeck()
+    const deckshaf = shuffle(decknotshaf)
+    for(let i = 0;i<52;i++){
+        if(i < 26){
+            player1.hand.push(deckshaf[i])
+        }else{
+            player2.hand.push(deckshaf[i])
+        }
+
+    }
+    const game = {
+        player1,
+        player2
+
+    }
+    return game
+}
+
+
+// console.log(initGame())
+
+function playRound(player_1, player_2){
+    while(player_1.hand.length >0){
+
+        let p1car = player_1.hand.shift()
+        let p2car = player_2.hand.shift()
+        let rezolt = compareCards(p1car,p2car)
+        if(rezolt == "p1"){
+            player_1.wonPile.push(p1car,p2car)
+        }
+        if(rezolt == "p2"){
+            player_2.wonPile.push(p1car,p2car)
+        }
+
+}
+if(player_1.wonPile.length>player_2.wonPile.length){
+    return "p1 wwwoooonnn"
+}
+if(player_1.wonPile.length<player_2.wonPile.length){
+    return "p2 wwwoooonnn"
+}
+
+}
+let obje = initGame()
+console.log(playRound(obje.player1,obje.player2))
+console.log(obje.player1.wonPile.length,obje.player2.wonPile.length)
 
 
 
-//     }
 
-// }
 
--- utils/deck.js
+++ utils/deck.js
@@ -1,75 +1,72 @@
-
-
-
-
-function createCard(rank, suite) {
-  const SPECIAL_RANK = { J: 11, Q: 12, K: 13, A: 14 };
-  if (rank in SPECIAL_RANK) {
-    value = SPECIAL_RANK[rank];
-  } else {
-    value = Number(rank);
-  }
-  const card = {
-    rank,
-    suite,
-    value,
-  };
-  return card;
-}
+export function createCard(rank, suite) {
+            let value = 0
+            const SPECIAL_RANK = { J: 11, Q: 12, K: 13, A: 14 };
+            if (rank in SPECIAL_RANK) {
+                value = SPECIAL_RANK[rank];
+            } else {
+                value = Number(rank);
+            }
+            const card = {
+                rank,
+                suite,
+                value
+            };
+            return card;
+            }
 // console.log(createCard("4","h"))
-function createDeck() {
-  const deckOfCards = [];
-  LIST_SUITE = ["H", "C", "D", "S"];
-  LIST_RANK = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "K",
-    "Q",
-    "A",
-  ];
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 13; j++) {
-      let cardd = createCard(LIST_RANK[j],LIST_SUITE[i])
-      deckOfCards.push(cardd)
-    }
-  }
-  return deckOfCards
-}
+export function createDeck() {
+            const deckOfCards = [];
+            const LIST_SUITE = ["H", "C", "D", "S"];
+            const LIST_RANK = [
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "J",
+                "K",
+                "Q",
+                "A",
+            ];
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < 13; j++) {
+                let cardd = createCard(LIST_RANK[j],LIST_SUITE[i])
+                deckOfCards.push(cardd)
+                }
+            }
+            return deckOfCards
+            }
 // console.log(createDeck())
 
 
-function shuffle(deck){
-    for(let i = 0;i<1000;i++){
-        let index1 = Math.floor(Math.random() * deck.length)
-        let index2 = Math.floor(Math.random() * deck.length)
-        if(index1 == index2){
-            continue
-        }else{
-            [deck[index2],deck[index1]] = [deck[index1],deck[index2]]
-        }
-    }
-    return deck
-}
+export function shuffle(deck){
+                for(let i = 0;i<1000;i++){
+                    let index1 = Math.floor(Math.random() * deck.length)
+                    let index2 = Math.floor(Math.random() * deck.length)
+                    if(index1 == index2){
+                        continue
+                    }else{
+                        [deck[index2],deck[index1]] = [deck[index1],deck[index2]]
+                    }
+                }
+                return deck
+            }
 
 // console.log(shuffle(createDeck()))
 
-function compareCards(p1Card, p2Card){
-    if(p1Card.value > p2Card.value){
-        return "p1"
-    }
-    if(p1Card.value < p2Card.value){
-        return "p2"
-    }
-    if(p1Card.value == p2Card.value){
-        return "WOR"
-    }
-}
+export function compareCards(p1Card, p2Card){
+            if(p1Card.value > p2Card.value){
+                return "p1"
+            }
+            if(p1Card.value < p2Card.value){
+                return "p2"
+            }
+            if(p1Card.value == p2Card.value){
+                return "WOR"
+            }
+        }
 // console.log(compareCards({ rank: 'A', suite: 'C', value: 9 }, { rank: '10', suite: 'S', value: 9 }))
